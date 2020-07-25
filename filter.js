67b664af9964fa2dc4ad4c6bd6c76aa3ee9eaf4b
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-07-25 23:04:35 +0200

    added filter and map methods

--- filter.js
+++ filter.js
@@ -1,1 +1,23 @@
+// filter method
+const items = [
+    { name: 'bike', price: 100 },
+    { name: 'TV', price: 200 },
+    { name: 'album', price: 10 },
+    { name: 'book', price: 5 },
+    { name: 'phone', price: 300 },
+    { name: 'laptop', price: 500 },
+];
 
+const filteredItems = items.filter((item) => {
+    return item.price <= 100
+});
+
+// console.log(items);
+// console.log(filteredItems);
+
+// map method
+const itemNames = items.map((item) => {
+    return item.name
+});
+
+console.log(itemNames);
